07fd8523fcc9d4b9330792c60e4f28450ec15880
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-26 22:29:24 +0600

    Import delivered profile images.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './main.scss';
 import './assets/logo.png';
+import './assets/profile-1.jpg';
+import './assets/profile-2.jpg';
+import './assets/profile-3.jpg';
+import './assets/profile-4.jpg';
+import './assets/profile-5.jpg';
+import './assets/profile-6.jpg';
