9e7b29feae9ea1661a71459d4e5288d61462ab88
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-31 21:23:49 +0600

    Add functionality for sticky navbar.

--- src/modules/stickyNav.js
+++ src/modules/stickyNav.js
@@ -1,2 +1,22 @@
-const stickyNav = () => {};
+const heroSection = document.querySelector('.hero');
+
+const stickyNav = () => {
+  const obs = new IntersectionObserver(
+    (entries) => {
+      const ent = entries[0];
+      if (ent.isIntersecting === false) {
+        document.body.classList.add('sticky');
+      }
+      if (ent.isIntersecting === true) {
+        document.body.classList.remove('sticky');
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: '-80px',
+    },
+  );
+  obs.observe(heroSection);
+};
 export default stickyNav;
