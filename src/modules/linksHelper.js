fa78c92479cde795a8c5e09156a1f8ec71a480ca
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-31 21:03:36 +0600

    Add functionality to work with all links

--- src/modules/linksHelper.js
+++ src/modules/linksHelper.js
@@ -1,3 +1,12 @@
-const linksHelper = () => {};
+const allLinks = document.querySelectorAll('a:link');
+
+const linksHelper = () => {
+  allLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+    });
+  });
+};
 
 export default linksHelper;
