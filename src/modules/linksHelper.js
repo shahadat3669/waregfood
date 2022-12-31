50d4bee5eb4d815969ffec868048164eaa023bcf
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-31 21:04:34 +0600

    Update linksHelper to  work on href attribute equal to #.

--- src/modules/linksHelper.js
+++ src/modules/linksHelper.js
@@ -5,6 +5,13 @@ const linksHelper = () => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
       const href = link.getAttribute('href');
+
+      if (href === '#') {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }
     });
   });
 };
